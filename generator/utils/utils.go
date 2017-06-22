1363ed75f72592d171c46f844704f45c64f4e6b6
Jonas Rudlang <jonas.rudlang@gmail.com>
2017-06-23 01:00:29 +0200

    Review the comment block function #8
    The  syntax is no longer required
    Multiline commments now works as expected

--- generator/utils/utils.go
+++ generator/utils/utils.go
@@ -1,25 +1,57 @@
 package utils
 
 import (
+	"fmt"
 	"strings"
+	"unicode"
 
 	"github.com/graphql-go/graphql/language/ast"
 	"github.com/graphql-go/graphql/language/kinds"
 )
 
-// Line represents a line of text
+// Line represents a line of text in context of a byte array
 type Line struct {
-	Text  string
-	Start int
-	End   int
+	Start  int
+	End    int
+	Source []byte
 }
 
-// FindLine takes a byte array and a start position
+// Text gets the contents of the line
+func (line Line) Text() string {
+	return string(line.Source[line.Start:line.End])
+}
+
+// IsStartOfLine returns true if ind is inside the white space between \n and
+// any character
+func (line Line) IsStartOfLine(ind int) bool {
+	start := line.Start
+	if ind < line.Start || ind > line.End {
+		return false
+	}
+	for _, c := range line.Text() {
+		// fmt.Printf("Char: %c\n", c)
+		if unicode.IsSpace(rune(c)) == true {
+			start++
+			continue
+		}
+
+		// fmt.Printf("Ind: %d, Start: %d\n", ind, start)
+		if ind <= start {
+			return true
+		}
+
+		break
+	}
+	return false
+}
+
+// GetLine takes a byte array and a start position
 // returns a line
-func FindLine(src []byte, start int) Line {
+func GetLine(src []byte, start int) (Line, error) {
 	var lineStart, lineEnd int
 	l := len(src)
 
+	// Find the end of the line
 	for i := start; i < l; i++ {
 		if src[i] == '\n' {
 			lineEnd = i
@@ -27,6 +59,7 @@ func FindLine(src []byte, start int) Line {
 		}
 	}
 
+	// Find the beginning of the line
 	for i := start; i > 0; i-- {
 		if src[i] == '\n' {
 			lineStart = i
@@ -34,11 +67,15 @@ func FindLine(src []byte, start int) Line {
 		}
 	}
 
-	return Line{
-		Text:  string(src[lineStart:lineEnd]),
-		Start: lineStart,
-		End:   lineEnd,
+	if lineEnd-lineStart == 0 {
+		return Line{}, fmt.Errorf("Empty line")
 	}
+
+	return Line{
+		Source: src,
+		Start:  lineStart,
+		End:    lineEnd,
+	}, nil
 }
 
 // GetCommentBlock takes a byte array and a start position
@@ -46,24 +83,39 @@ func FindLine(src []byte, start int) Line {
 func GetCommentBlock(src []byte, start int) []string {
 
 	pos := start
-	gap := 0
+	linegap := 0
 	var comments []string
 
-	for {
-		line := FindLine(src, pos)
-		trimLine := strings.TrimSpace(line.Text)
+	line, err := GetLine(src, pos)
+	if err != nil {
+		return nil
+	}
 
-		if strings.HasPrefix(trimLine, "# - ") {
-			block := strings.TrimLeft(trimLine, "# - ")
-			comments = append([]string{block}, comments...)
-			// fmt.Println(block)
-		} else if len(trimLine) > 0 {
-			if gap > 0 {
-				break
-			}
-			gap++
+	if line.IsStartOfLine(start) == false {
+		return nil
+	}
+
+	for {
+		line, err := GetLine(src, pos)
+		if err != nil {
+			break
 		}
+
+		trimLine := strings.TrimSpace(line.Text())
+
+		if strings.HasPrefix(trimLine, "#") {
+			block := strings.TrimSpace(strings.TrimLeft(trimLine, "#"))
+			comments = append([]string{block}, comments...)
+			linegap = 0
+		}
+
+		if linegap > 0 {
+			break
+		}
+
 		pos = (line.Start - 1)
+		linegap++
+
 		if pos <= 0 {
 			break
 		}
