b735b92d8dd47cf288b75702b34a1c3acb67f967
Jonas Rudlang <jonas.rudlang@gmail.com>
2017-06-24 22:35:04 +0200

    Add function LineCounter

--- generator/utils/utils.go
+++ generator/utils/utils.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"strings"
 	"unicode"
 
@@ -132,5 +134,25 @@ func ParseType(t string, loc *ast.Location) ast.Type {
 	return &ast.Named{
 		Kind: kinds.Named,
 		Loc:  loc,
+	}
+}
+
+// LineCounter counts lines in a string/byte buffer
+func LineCounter(r io.Reader) (int, error) {
+	buf := make([]byte, 32*1024)
+	count := 0
+	lineSep := []byte{'\n'}
+
+	for {
+		c, err := r.Read(buf)
+		count += bytes.Count(buf[:c], lineSep)
+
+		switch {
+		case err == io.EOF:
+			return count, nil
+
+		case err != nil:
+			return count, err
+		}
 	}
 }
